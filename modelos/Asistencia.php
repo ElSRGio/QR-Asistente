9dde340b0459d60271c034a4a9a22c46ad4b46ee
JuanGPerezz <128273014+JuanGPerezz@users.noreply.github.com>
2024-04-19 09:34:56 -0600

    nueva clase

---
+++ modelos/Asistencia.php
@@ -0,0 +1,22 @@
+<?php
+// Incluir la conexión a la base de datos 
+require "../config/conexion.php";
+
+class Asistencia
+{
+
+//Implemetamos nuestro construtor
+
+public function_construct()
+
+{   
+}
+
+//Listas usarios
+
+public listar()
+
+
+
+
+}

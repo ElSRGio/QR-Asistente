4ea41d1597387d532b65af1c66390fb1c786fd01
JuanGPerezz <128273014+JuanGPerezz@users.noreply.github.com>
2024-04-20 10:43:41 -0600

    Asistencia full

--- modelos/Asistencia.php
+++ modelos/Asistencia.php
@@ -4,28 +4,29 @@ require "../config/conexion.php";
 
 class Asistencia
 {
-
-//Implemetamos nuestro construtor
-
-public function_construct()
-
-{   
-}
-
-//Listas Registro
-public function listar()
-{
-$sql = "SELECT a.', CONCAT(e.nombre,' ',e.apellidos) AS empleado, e.codido FROM asistencia a INNER JOIN empleado e ON a.
-empleado_id=e.id ORDER BY a.id DESC;
-return ejecutarConsulta($sql);
-}
-
-public function listar_repoter($fecha_inicio, $fecha_fin, $empelado_id)
-{
-
-$sql = SELECT a.', CONCAT(e.nombre,' ',e.apellidos) AS empleado, e.codido FROM asistencia a INNER JOIN empleado e ON a.
-empleado_id=e.id ORDER BY a.id DESC;
-
+    // Implemetamos nuestro construtor
+    public function __construct()
+    {   
     }
 
+    // Listas Registro
+    public function listar()
+    {
+
+    $sql = "SELECT a.', CONCAT(e.nombre,' ',e.apellidos) AS empleado, e.codigo FROM asistencia a INNER JOIN empleado e ON a.
+    empleado_id=e.id ORDER BY a.id DESC";
+    return ejecutarConsulta($sql);
+    }
+
+    public function listar_repoter($fecha_inicio, $fecha_fin, $empelado_id)
+    {
+        $sql = "SELECT a.', CONCAT(e.nombre,' ',e.apellidos) AS empleado, e.codigo FROM asistencia a INNER JOIN empleado e ON  a.
+        empleado_id=e.id WHERE DATE(a.fecha)>= '$fecha_inicio' AND DATE(a.fecha)<='$fecha_fin' AND a.empleado_id='$empelado_id'"; 
+        return ejecutarConsulta($sql);
+    }
 }
+
+
+
+
+

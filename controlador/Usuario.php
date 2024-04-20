f406c397a73cacee97769f725932bbc2163e981e
JuanGPerezz <128273014+JuanGPerezz@users.noreply.github.com>
2024-04-20 12:05:02 -0600

    cambios

---
+++ controlador/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+// Iniciamos la sesión
+session_start();
+// Incluimos el archivo de la clase Usuario 
+require_once "../modelos/Usuario.php";
+
+// Creamos una instancia del objeto
+$usuario Susuario=new Usuario();
+
+// Recibimos los datos enviados por el formulario
+$idusuario = isset($_POST["idusuario"]) ? limpiarCadena($_POST["idusuario"]) : "";
+$nombre = isset($_POST["nombre"]) ? limpiarCadena($_POST["nombre"]) : "";
+$login = isset($_POST["login"]) ? limpiarCadena($_POST["login"]) : "";
+$email = isset($_POST["email"]) ? limpiarCadena($_POST["email"]) : "";
+$password = isset($_POST["password"]) ? limpiarCadena($_POST["password"]) : "";
+$password = isset($_POST["password"]) ? limpiarCadena($_POST["password"]) : "";
+$imagen = isset($_POST["imagen"]) ? limpiarCadena($_POST["imagen"]) : "";
